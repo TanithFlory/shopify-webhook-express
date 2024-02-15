d1bcf464c9b4a467aac5ad33a537d24693052b57
TanithFlory <tanithflory7770@gmail.com>
2024-02-15 16:25:40 +0530

    build ver

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+app.get("/", (_req, res) => {
+    res.send("/");
+});
+app.get("/orders-paid", (req, _res) => {
+    const body = req.body;
+    const { id, current_subtotal_price, order_number, customer } = body;
+    const { first_name, last_name, state, email, phone, default_address } = customer;
+    const { address1, address2, city, country, zip } = default_address;
+});
+app.get("/ping", (_req, res) => {
+    return res.send("pong 🏓");
+});
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+});
+//# sourceMappingURL=index.js.map
