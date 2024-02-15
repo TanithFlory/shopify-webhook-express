6eac9ec37d658b54048b80df14e8f4b6a446791f
TanithFlory <tanithflory7770@gmail.com>
2024-02-15 16:23:09 +0530

    ts

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("/");
+});
+
+app.get("/orders-paid", (req: Request, _res: Response) => {
+  const body = req.body;
+
+  const { id, current_subtotal_price, order_number, customer } = body;
+
+  const { first_name, last_name, state, email, phone, default_address } =
+    customer;
+
+  const { address1, address2, city, country, zip } = default_address;
+});
+
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
