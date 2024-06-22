b3d076ba9ba8afe8136a89d61c432ed9747af271
TanithFlory <tanithflory7770@gmail.com>
2024-06-22 15:46:46 +0530

    Add: bundles

---
+++ dist/utils/getInstallationDetails.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getInstallationDetails(line_items, shipping_address, order_number) {
+    const { first_name, last_name, address1, address2, city, zip, phone, province, } = shipping_address;
+    const today = new Date();
+    const year = today.getFullYear().toString();
+    const day = today.getDate().toString().padStart(2, "0");
+    let month = today.getMonth() + 1;
+    month = (month + 1) % 12;
+    if (month === 0) {
+        month = 12;
+    }
+    const customerPersonDetails = {
+        cust_full_name: `${first_name} ${last_name}`,
+        cust_mobile: phone
+            .replace(/[-\s]/g, "")
+            .replace(/^(\+91)/, "")
+            .trim(),
+        cust_city: city,
+        cust_line_0: "",
+        cust_line_1: address1,
+        cust_line_2: address2,
+        cust_pincode: zip,
+        cust_state: province,
+        request_req_date: `${year}-${month.toString().padStart(2, "0")}-${day}`,
+        request_priority: "Normal",
+    };
+    const installationDetails = {
+        batch_data: [],
+    };
+    let installationRequired = false;
+    let isASmartLock = false;
+    for (const item of line_items) {
+        let title = item.title;
+        const isADoorLock = title.toLowerCase().includes("smart") &&
+            title.toLowerCase().includes("lock");
+        if (!isASmartLock) {
+            isASmartLock = isADoorLock;
+        }
+        if (Object.prototype.hasOwnProperty.call(bundles, title)) {
+            title = bundles[title];
+        }
+        if (isADoorLock) {
+            installationDetails.batch_data.push(Object.assign(Object.assign({}, customerPersonDetails), { request_description: `${order_number.toString()} - ${title} - installation`, "79a88c7b-c64f-46c4-a277-bc80efa1c154": `${item.id}` }));
+            continue;
+        }
+        if (!installationRequired) {
+            installationRequired = title.toLowerCase().includes("free installation");
+        }
+    }
+    return { installationRequired, installationDetails, isASmartLock };
+}
+exports.default = getInstallationDetails;
+const bundles = Object.create({
+    "Traditional Door Security Bundle": "Aqara Smart Door Lock A100 Zigbee",
+    "Advanced Door Security Bundle": "Aqara Smart Door Lock D100 Zigbee",
+    "Ultimate Door Security Package": "Aqara Smart Lock D200i",
+    "Affordable Door Security Bundle": "Aqara Smart Lock U100 (Kit includes Aqara E1 Hub)",
+});
+//# sourceMappingURL=getInstallationDetails.js.map
